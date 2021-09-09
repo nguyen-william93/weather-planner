cd83231e1fe5ad007fb95d75e23cc55be5103f9a
nguyen-william93 <=>
2021-09-09 18:48:34 -0500

    testing out api call

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+
+var getWeather = function(lon, lat){
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=9698d78e4b0b91d10c1cae15ee7197eb"
+    var response = fetch(apiUrl).then(function(response){
+        if (response.ok){
+            response.json().then(function(data){
+                console.log(data);
+            })
+        }
+    })
+}
+
+var getLatLon = function(city){
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=9698d78e4b0b91d10c1cae15ee7197eb"
+
+    var response = fetch(apiUrl).then(function(response){
+        if (response.ok){
+            response.json().then(function(data){
+                getWeather(data.coord.lon, data.coord.lat);
+            })
+        }
+    })
+}
+
+$("#search").on("click", function(event){
+    event.preventDefault();
+    var cityName = $("#city").val();
+    getLatLon(cityName);
+});
